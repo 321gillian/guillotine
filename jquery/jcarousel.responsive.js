f489accaeb5bb2667b6e549e697bea958d3db38b
321gillian <gillian.middleton@gmail.com>
2017-11-29 13:30:47 +0000

    Change the background for the gallery to polaroid.
    Added in the linked captions on the board.

--- jquery/jcarousel.responsive.js
+++ jquery/jcarousel.responsive.js
@@ -21,29 +21,14 @@
 
         $('.jcarousel-control-prev')
             .jcarouselControl({
-                target: '-=1'
+                target: '-=2'
             });
 
         $('.jcarousel-control-next')
             .jcarouselControl({
-                target: '+=1'
+                target: '+=2'
             });
 
-        $('.jcarousel-pagination')
-            .on('jcarouselpagination:active', 'a', function() {
-                $(this).addClass('active');
-            })
-            .on('jcarouselpagination:inactive', 'a', function() {
-                $(this).removeClass('active');
-            })
-            .on('click', function(e) {
-                e.preventDefault();
-            })
-            .jcarouselPagination({
-                perPage: 1,
-                item: function(page) {
-                    return '<a href="#' + page + '">' + page + '</a>';
-                }
-            });
+
     });
 })(jQuery);
